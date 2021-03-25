ad45896549c7b8230b56b833152117e4a0f726b3
jbdesigns88 <jbdesigns88@gmail.com>
2021-03-24 23:30:16 -0400

    upadted javascript, cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,82 +1,71 @@
-$(function() {
-
+(function() {
     const team = [{
-            image: "./images/image-tanya.jpg",
-            info: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ” ',
-            name: "Tanya Sinclair ",
-            title: "UX Engineer"
-        },
-
-        {
-            image: "./images/image-john.jpg",
-            info: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.  ” ',
-            name: "John Tarkpor",
-            title: "Junior Front-end Developer"
-        },
-
-    ];
+        image: "./images/image-tanya.jpg",
+        info: '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ” ',
+        name: "Tanya Sinclair ",
+        title: "UX Engineer"
+    }, {
+        image: "./images/image-john.jpg",
+        info: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.  ” ',
+        name: "John Tarkpor",
+        title: "Junior Front-end Developer"
+    }, ];
 
     let current = 0;
-    const prev = $(".previous")
-    const next = $(".next");
-    const info = $(".info");
-    const employee = $(".employee");
-    const title = $(".title");
-    const image = $(".user-image");
+    const prev = document.querySelector(".previous");
+    const next = document.querySelector(".next");
+    const info = document.querySelector(".info");
+    const employee = document.querySelector(".employee");
+    const title = document.querySelector(".title");
+    const image = document.querySelector(".user-image");
 
+    document.addEventListener("keydown", function(event) {
+        event.preventDefault();
+        let goToNext = event.key.toLowerCase() === "right" || event.key.toLowerCase() === "arrowright";
+        let goToPrevious = event.key.toLowerCase() === "left" || event.key.toLowerCase() === "arrowleft"
 
-
-    try {
-        document.addEventListener("keydown", function(event) {
-
-            if (event.which === 39) {
-                changeToNextSlide()
-            }
-            if (event.which === 37) {
-                changeToPreviousSlide()
-            }
-        })
-
-    } catch (error) {
-        console.log("IE issues")
-    }
-
-    prev.click(function() {
-
-        changeToPreviousSlide()
-    })
-
-    next.click(function() {
-        changeToNextSlide()
-
-    })
-
-    function changeToNextSlide() {
-        increment();
-        update();
-    }
-
-    function changeToPreviousSlide() {
-        decrement()
-        update();
-    }
-
-    function update() {
-        changeInfo()
-        changeEmployee()
-        changeTitle()
-        changeImage()
-    }
-
-    function increment() {
-        if (current === team.length - 1) {
-            reset();
+        if (goToNext) {
+            goToNextSlide();
             return;
         }
-        current++
+
+        if (goToPrevious) {
+            goToPreviousSlide();
+            return;
+        }
+
+    });
+
+    prev.addEventListener("click", function(event) {
+        event.preventDefault();
+        goToPreviousSlide();
+    });
+
+    next.addEventListener("click", function(event) {
+        event.preventDefault();
+        goToNextSlide();
+    });
+
+
+    function goToNextSlide() {
+        incrementCurrent();
+        update();
     }
 
-    function decrement() {
+    function goToPreviousSlide() {
+        decrementCurrent();
+        update();
+    }
+
+    function incrementCurrent() {
+        if (current === team.length - 1) {
+            resetCurrent();
+            return;
+        }
+        current++;
+    }
+
+    function decrementCurrent() {
         if (current === 0) {
             current = team.length - 1;
             return;
@@ -84,26 +73,31 @@ $(function() {
         current--;
     }
 
-    function reset() {
+    function resetCurrent() {
         current = 0;
     }
 
+    function update() {
+        changeInfo();
+        changeEmployee();
+        changeTitle();
+        changeImage();
+    }
+
     function changeInfo() {
-        console.dir(info)
-        info.text(team[current].info)
+        info.innerText = team[current].info;
     }
 
     function changeEmployee() {
-        employee.text(team[current].name)
+        employee.innerText = team[current].name
     }
 
     function changeTitle() {
-        title.text(team[current].title)
+        title.innerText = team[current].title
     }
 
     function changeImage() {
-        image.attr("src", team[current].image);
+        image.setAttribute("src", team[current].image)
     }
 
-
-});
+})();
